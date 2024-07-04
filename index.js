56352358157ee4c9fcdfcdeed9b1a96175aa8545
Andrew SungWoo Chun <andrewchun041@gmail.com>
2024-07-04 12:59:29 -0700

    set up index.js and seperate style from index.html to styles.css; add js for seconds hand movement for current time

---
+++ index.js
@@ -0,0 +1,15 @@
+const secondHand = document.querySelector('.second-hand');
+const minHnad = document.querySelector('.min-hand');
+
+function setDate() {
+    const now = new Date();
+    const seconds = now.getSeconds();
+    const secondsDegrees = ((seconds / 60) * 360) + 90;
+    secondHand.style.transform = `rotate(${secondsDegrees}deg)`
+
+    const min = now.getMinutes();
+    const minDegrees = ((min / 60) * 360) + 90;
+    
+}
+
+setInterval(setDate, 1000);

2538005e7167aefb5555f0822e930c3e0911273e
Andrew SungWoo Chun <andrewchun041@gmail.com>
2024-07-04 14:04:49 -0700

    add minute hand movement

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ const minHnad = document.querySelector('.min-hand');
 
 function setDate() {
     const now = new Date();
+
     const seconds = now.getSeconds();
     const secondsDegrees = ((seconds / 60) * 360) + 90;
-    secondHand.style.transform = `rotate(${secondsDegrees}deg)`
+    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
 
     const min = now.getMinutes();
     const minDegrees = ((min / 60) * 360) + 90;
+    minHnad.style.transform = `rotate(${minDegrees}deg)`
     
 }
 
